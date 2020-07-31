e3d6741ff7e6306cf1ab79a37144ca2bb733ebef
Stevelsa <13750555642@163.com>
2020-07-31 18:50:29 +0800

    add Spring files on E430

---
+++ ForumserverApplication.java
@@ -0,0 +1,11 @@
+package com.dtt.forum.forumserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumserverApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ForumserverApplication.class, args);
+	}
+}
